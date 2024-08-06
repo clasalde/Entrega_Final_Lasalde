55061c714d3e004fb0b92f54ca347cacabcbf2da
Claudio Lasalde <calasalde@gmail.com>
2024-08-06 10:45:04 -0300

    correccion mail de compra

--- src/services/carts.services.js
+++ src/services/carts.services.js
@@ -1,13 +1,167 @@
+// const CartsRepository = require("../repositories/carts.repository.js");
+// const ProductsRepository = require("../repositories/products.repository.js");
+// const TicketsService = require("./tickets.service.js");
+// const UserModel = require("../models/user.model.js");
+// const EmailManager = require("../utils/email.js"); 
+// const { generateTicketCode } = require("../utils/utils.js");
+
+// const ticketsService = new TicketsService();
+// const cartsRepository = new CartsRepository();
+// const productsRepository = new ProductsRepository();
+
+// class CartsServices {
+//   constructor() {
+//     this.emailManager = new EmailManager();
+//   }
+
+//   async getCartById(id) {
+//     return await cartsRepository.getCartById(id, true);
+//   }
+
+//   async createCart() {
+//     return await cartsRepository.createCart();
+//   }
+
+//   async deleteProductsFromCart(cid) {
+//     const cart = await cartsRepository.getCartById(cid, false);
+//     if (!cart) {
+//       throw "Cart to update not found";
+//     } else {
+//       cart.products = [];
+//       await cart.save();
+//       return cart;
+//     }
+//   }
+
+//   async addToCart(cid, pid, quantity) {
+//     const cart = await cartsRepository.getCartById(cid, false);
+//     if (!cart) {
+//       throw "Cart to update not found";
+//     }
+//     const productExists = cart.products.find(
+//       (product) => product.product.toString() == pid
+//     );
+//     if (productExists) {
+//       productExists.quantity = productExists.quantity + quantity;
+//     } else {
+//       cart.products.push({ product: pid, quantity: quantity });
+//     }
+//     cart.markModified("products");
+//     await cart.save();
+//     return cart;
+//   }
+
+//   async deleteFromCart(cartId, productId) {
+//     const cart = await cartsRepository.getCartById(cartId, false);
+//     if (!cart) {
+//       throw "Cart to update not found";
+//     } else {
+//       cart.products = cart.products.filter((e) => e.product != productId);
+//       cart.markModified("products");
+//       await cart.save();
+//       return cart;
+//     }
+//   }
+
+//   async addProductsToCart(cartId, products) {
+//     const cart = await cartsRepository.getCartById(cartId, false);
+//     if (!cart) {
+//       throw "Cart to update not found";
+//     }
+//     const productsToSend = products.map((e) => {
+//       return { product: e._id, quantity: e.quantity || 1 };
+//     });
+
+//     productsToSend.forEach((product) => {
+//       const productExists = cart.products.find(
+//         (a) => a.product.toString() == product.product
+//       );
+//       if (productExists) {
+//         productExists.quantity = productExists.quantity + product.quantity;
+//       } else {
+//         cart.products.push(product);
+//       }
+//     });
+//     cart.markModified("products");
+//     await cart.save();
+//     return cart;
+//   }
+
+//   async finishPurchase(cartId) {
+//     console.log(`Iniciando proceso de compra para carrito: ${cartId}`);
+//     const cart = await cartsRepository.getCartById(cartId, false);
+//     const user = await UserModel.findOne({ cart: cartId });
+//     console.log(`Usuario encontrado:`, user);
+
+//     let failedPurchaseProducts = [];
+//     let leftOverProductsForTicket = [];
+//     let total = 0;
+//     for (const cartProduct of cart.products) {
+//       const product = await productsRepository.getProductByIdNoLean(
+//         cartProduct.product
+//       );
+//       if (product) {
+//         if (product.stock >= cartProduct.quantity) {
+//           product.stock = product.stock - cartProduct.quantity;
+//           total += cartProduct.quantity * product.price;
+//           product.save();
+//         } else {
+//           failedPurchaseProducts.push(cartProduct.product);
+//           leftOverProductsForTicket.push(cartProduct);
+//         }
+//       }
+//     }
+//     cart.products = leftOverProductsForTicket;
+//     cart.markModified("products");
+//     await cart.save();
+
+//     const ticketData = {
+//       code: generateTicketCode(),
+//       purchase_datetime: new Date(),
+//       amount: total,
+//       purchaser: user.email,
+//       purchaserId: user._id,
+//     };
+
+//     const ticket = await ticketsService.createTicket(ticketData);
+//     console.log(`Ticket creado:`, ticket);
+
+//     if (user && user.email) {
+//     try {
+//       console.log(`Intentando enviar correo a: ${user.email}`);
+//         await this.emailManager.enviarCorreoCompra(
+//           user.email,
+//           user.first_name,
+//           ticket.code
+//         );
+//         console.log(`Correo enviado con éxito a: ${user.email}`);
+//       } catch (error) {
+//       console.error("Error al enviar correo de confirmación:", error);
+//     }
+//     return ticket;
+//   }
+// }
+
+// module.exports = CartsServices;
+
+
+
 const CartsRepository = require("../repositories/carts.repository.js");
 const ProductsRepository = require("../repositories/products.repository.js");
 const TicketsService = require("./tickets.service.js");
 const UserModel = require("../models/user.model.js");
+const EmailManager = require("../helpers/email.js");
+const {generateTicketCode} = require("../utils/utils.js");
+
 const ticketsService = new TicketsService();
 const cartsRepository = new CartsRepository();
 const productsRepository = new ProductsRepository();
-const { generateTicketCode } = require("../utils/utils.js");
 
 class CartsServices {
+  constructor() {
+    this.emailManager = new EmailManager();
+  }
+
   async getCartById(id) {
     return await cartsRepository.getCartById(id, true);
   }
@@ -82,8 +236,10 @@ class CartsServices {
   }
 
   async finishPurchase(cartId) {
+    console.log(`Iniciando proceso de compra para carrito: ${cartId}`);
     const cart = await cartsRepository.getCartById(cartId, false);
     const user = await UserModel.findOne({ cart: cartId });
+    console.log(`Usuario encontrado:`, user);
     let failedPurchaseProducts = [];
     let leftOverProductsForTicket = [];
     let total = 0;
@@ -110,20 +266,24 @@ class CartsServices {
       purchase_datetime: new Date(),
       amount: total,
       purchaser: user.email,
-      purchaserId: user._id,
-    };
-    const ticket = await ticketsService.createTicket(ticketData);
-    try {
-      if (user && user.email) {
-        await emailManager.enviarCorreoCompra(
-          user.email,
-          user.first_name,
-          ticket.code
-        );
-      }
-    } catch (error) {
-      console.error("Error al enviar correo de confirmación:", error);
+      purchaserId: user._id
     }
+    const ticket = await ticketsService.createTicket(ticketData)
+    console.log(`Ticket creado:`, ticket);
+
+    if (user && user.email) {
+      try {
+        console.log(`Intentando enviar correo a: ${user.email}`);
+        await this.emailManager.enviarCorreoCompra(user.email, user.first_name, ticket.code);
+        console.log(`Correo enviado con éxito a: ${user.email}`);
+      } catch (error) {
+        console.error(`Error al enviar correo a ${user.email}:`, error);
+        // Aquí puedes decidir si quieres lanzar el error o simplemente registrarlo
+      }
+    } else {
+      console.log(`No se pudo enviar correo. Usuario o email no disponible.`);
+    }
+
     return ticket;
   }
 }
